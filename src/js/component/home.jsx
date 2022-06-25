337f1321de6fc54e7172f4c1c460ec581e2a07b9
Brox13 <a_gomez@hotmail.es>
2022-06-25 17:39:44 +0000

    archivo final limpio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-
 const Home = () => {
 	return (
 		<div>
 			<CrearSemaforo />
-			
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,8 @@
 //import react into the bundle
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
-
-const Luz = {
-	boxShadow: "10px",
-};
-
 
 const CrearSemaforo = () =>{
     const [estadocolor, setEstadolight] = useState(null);
@@ -29,43 +20,4 @@ const CrearSemaforo = () =>{
         </>
 )};
 
-
-
-// const Home = () => {
-//     const [color, brillo]=useState(null);
-//     return (
-//         <div className="container-fluid">
-//             <div className="palito">
-//                 <div className="luces">
-//                     <div className={ color==="red"? "red luzenc": "red"}   onClick = {()=>brillo("red")}></div>
-//                     <div className={ color==="yellow"? "yellow luzenc": "yellow"}onClick = {()=>brillo("yellow")}></div>
-//                     <div className={ color==="green"? "green luzenc": "green"}onClick = {()=>brillo("green")}></div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-//         }
-
-
-// const Semaforo = () =>{
-//     const colores = ["rojo", "naranja", "verde"];
-//     const [estado, setEstado] = useState("rojo");
-//     return (
-//         <div className="CrearSemaforo">
-//             colores.map(crearsemaforo) {
-
-//             }
-//         </div>
-//     );
-// };
-
-// const Colores = () =>{
-//     return(
-//         <div className="elegircolor">
-//             <
-//         </div>
-//     )
-
-// }
-//render your react application
 ReactDOM.render(<CrearSemaforo />, document.querySelector("#app"));
